20ab2bec655cbffff041ec2e2d248e1282f3557e
Baljinder Singh <baljindersingh5755@gmail.com>
2026-06-09 10:16:53 -0400

    latest feartures added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require('../models/User');
 const { signup, login } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
@@ -11,6 +12,23 @@ router.get('/me', protect, (req, res) => {
     res.status(200).json({
         user: req.user
     });
+});
+
+router.get('/profile', protect, async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id)
+            .select('_id name email');
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
